117738d758f7aef23c2e2053f2bb2939fd519284
Victor Gabriel <victor.g.filgueiras@gmail.com>
2023-01-31 17:20:09 -0300

    vercel config

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const testController = require('./controller/testController')
 
 router.post('/test', testController)
+router.get('/', (req, res) => res.send('Hello World!'))
 
 module.exports = router
