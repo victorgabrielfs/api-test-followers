eacfe91a4883b7ee6b4ea516071f6f7773092f2b
Victor Gabriel <victor.g.filgueiras@gmail.com>
2023-06-05 15:21:30 -0300

    maisuma

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const testController = require('./controller/testController')
 const multer = require('multer')
+const upload = multer()
 
-router.post('/test', multer.none(), testController)
+router.post('/test', upload.none(), testController)
 router.get('/', (req, res) => res.send('Hello World!'))
 
 module.exports = router
