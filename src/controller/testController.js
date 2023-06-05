b95c82235fc8038d11a4e3f291e46515d68d3610
Victor Gabriel <victor.g.filgueiras@gmail.com>
2023-06-05 16:32:57 -0300

    maisuma

--- src/controller/testController.js
+++ src/controller/testController.js
@@ -3,38 +3,37 @@ const axios = require('axios')
 
 const createTest = async (req, res) => {
 	const insta_user = req.body.fields.name.value
+	insta_user[0] === '@' ? insta_user.slice(1) : insta_user
 	const email = req.body.fields.email.value
 	const name = req.body.fields.name.value
 	console.log(insta_user)
 	console.log(email)
 	console.log(name)
-	console.log(req.body)
 
 	console.log('chegou aqui')
-	return res.send(JSON.stringify(req.body))
 
-	// if (await TestData.findOne({ insta_user })) {
-	// 	return res.status(400).json({ error: 'Insta profile already tested' })
-	// }
+	if (await TestData.findOne({ insta_user })) {
+		return res.status(400).json({ error: 'Insta profile already tested' })
+	}
 
-	// const url = 'https://painelseguidores.com.br/api/v2'
-	// axios
-	// 	.post(url, {
-	// 		key: process.env.API_KEY,
-	// 		action: 'add',
-	// 		service: '207',
-	// 		link: `http://instagram.com/${insta_user}`,
-	// 		quantity: 20
-	// 	})
-	// 	.then(async (response) => {
-	// 		console.log(response.data)
-	// 		const user = await TestData.create({ name, email, insta_user })
-	// 		return res.send({ user })
-	// 	})
-	// 	.catch((error) => {
-	// 		console.log(error)
-	// 		return res.send({ error: 'Error on request' })
-	// 	})
+	const url = 'https://agenciapopular.com/api/v2'
+	axios
+		.post(url, {
+			key: process.env.API_KEY,
+			action: 'add',
+			service: '143',
+			link: `http://instagram.com/${insta_user}`,
+			quantity: 10
+		})
+		.then(async (response) => {
+			console.log(response.data)
+			const user = await TestData.create({ name, email, insta_user })
+			return res.send({ user })
+		})
+		.catch((error) => {
+			console.log(error)
+			return res.send({ error: 'Error on request' })
+		})
 }
 
 module.exports = createTest
