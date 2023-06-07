b335ebd47539d571457983c7bba58eb1d0f1c414
Victor Gabriel <victor.g.filgueiras@gmail.com>
2023-06-07 13:32:07 -0300

    mais um

--- src/controller/testController.js
+++ src/controller/testController.js
@@ -2,6 +2,8 @@ const TestData = require('../model/Test')
 const axios = require('axios')
 
 const createTest = async (req, res) => {
+	console.log(req)
+
 	let insta_user = req.body.fields.insta_user.value
 	insta_user[0] === '@' ? (insta_user = insta_user.slice(1)) : insta_user
 	const email = req.body.fields.email.value
